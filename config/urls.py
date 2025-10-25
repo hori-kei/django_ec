860aa500673daf40053bce30154008f428c43ba7
hori-kei <keichan990209@gmail.com>
2025-10-25 15:44:47 +0900

    Fix:config.urls.pyとproducts.urls.pyのタイポ修正

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('hello/', TemplateView.as_view(template_name='hello.html')),
-    path('products/', include("products.urls"))
+    path("admin/", admin.site.urls),
+    path("hello/", TemplateView.as_view(template_name="hello.html")),
+    path("products/", include("products.urls")),
 ]
--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+
 from .views import ProductListView
 
 app_name = "products"
 
 urlpatterns = [
-    path('', ProductListView.as_view(), name="product_list")
+    path("", ProductListView.as_view(), name="product_list"),
 ]
