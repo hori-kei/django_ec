a83690ff0d3ddb65ba582493bd02c4ac92fd4bb1
hori-kei <keichan990209@gmail.com>
2025-12-07 20:48:41 +0900

    Add Product model and seed initial product data

--- product/models.py
+++ product/models.py
@@ -1,3 +1,15 @@
+# Create your models here.
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    class Meta:
+        db_table = "product"
+
+    name = models.CharField("商品名", max_length=100)
+    price = models.IntegerField("価格", default=0)
+    image = models.CharField("画像", max_length=255)
+    created_at = models.DateTimeField("掲載日", auto_now_add=True)
+
+    def __str__(self):
+        return self.name
