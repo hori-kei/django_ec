17669bca0060a4ca6d687f776813f2fde4492e18
hori-kei <keichan990209@gmail.com>
2025-12-16 09:38:48 +0900

    ProductDetail用のview作成

--- product/views.py
+++ product/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 
 from .models import Product
 
@@ -13,3 +13,9 @@ class ProductListView(ListView):
 
     def get_queryset(self):
         return Product.objects.order_by("-created_at")
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = "product/product_detail.html"
+    context_object_name = "product"
