ffd655345227443f4c1779274cd4c42908966f6f
hori-kei <keichan990209@gmail.com>
2025-12-16 09:39:25 +0900

    ProductDetail用のurlを追記

--- product/urls.py
+++ product/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import ProductListView
+from .views import ProductDetailView, ProductListView
 
 app_name = "product"
 
 urlpatterns = [
     path("", ProductListView.as_view(), name="product_list"),
+    path("<int:pk>/", ProductDetailView.as_view(), name="product_detail"),
 ]
