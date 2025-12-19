500d30d491b2f641ec191693336b2e5643d74e10
hori-kei <keichan990209@gmail.com>
2025-12-19 17:46:25 +0900

    商品管理：登録・編集機能を実装

--- control/urls.py
+++ control/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 
-from .views import ManageListView, ProductCreateView
+from .views import (  # ProductDeleteView,
+    ManageListView,
+    ProductCreateView,
+    ProductUpdateView,
+)
 
 app_name = "control"
 
 urlpatterns = [
     path("products/", ManageListView.as_view(), name="manage_list"),
     path("products/create/", ProductCreateView.as_view(), name="manage_create"),
+    path("products/<int:pk>/edit/", ProductUpdateView.as_view(), name="manage_edit"),
+    # path("products/<int:pk>/delete/", ProductDeleteView.as_view(), name="manage_delete"),
 ]
--- control/views.py
+++ control/views.py
@@ -1,5 +1,5 @@
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, DeleteView, ListView, UpdateView
 
 from product.models import Product
 
@@ -8,10 +8,18 @@ class ManageListView(ListView):
     model = Product
     template_name = "control/manage_list.html"
     context_object_name = "products"
+    ordering = ["pk"]
 
 
 class ProductCreateView(CreateView):
     model = Product
-    fields = ["name", "price", "image"]
+    fields = ("name", "price", "image")
+    template_name = "control/manage_create.html"
+    success_url = reverse_lazy("control:manage_list")
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    fields = ("name", "price", "image")
     template_name = "control/manage_create.html"
     success_url = reverse_lazy("control:manage_list")
